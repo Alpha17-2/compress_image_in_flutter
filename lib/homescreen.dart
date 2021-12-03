e3102431494d2942fbafb9e6173147092c906158
Alpha17-2 <alphaisgod1@gmail.com>
2021-12-03 14:40:51 +0530

    Introduced flutter_image_compress package

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
